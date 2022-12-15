cdf0fc1d0412e92f45de5c78ccde19321a1e362f
itfang <fangjiaxing3@gmail.com>
2022-12-15 14:30:19 +0800

    hot-fix commit

--- git-test/src/main/java/org/example/Main.java
+++ git-test/src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello git!");
+        System.out.println("Hello git1");
         System.out.println("Hello git2");
         System.out.println("Hello git3");
     }
